b211d039ec9237250cb99e3c39305bd10e10566e
gsack <gsackie@iu.edu>
2024-04-14 23:15:46 -0400

    HW8 changes

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode=0;
 
 const host_local = 'http://localhost:8080';
-const host_remote= "https://ducks-service-???.onrender.com";
+//const host_remote= "https://ducks-service-???.onrender.com";
 
 function getHost(){
     retrn (mode==0)? host_local : host_remote;
